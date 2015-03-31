e21f7202d483ad3df07d1d396226d2f0a36005d4
Alexey Chernysh <alexey.chernysh@gmail.com>
2015-03-31 15:28:30 +0700

    automatic tests added

---
+++ app/src/main/java/ru/android_cnc/acnc/Geometry/CNCPointTest.java
@@ -0,0 +1,95 @@
+package ru.android_cnc.acnc.Geometry;
+
+import junit.framework.TestCase;
+
+public class CNCPointTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testGetX() throws Exception {
+
+    }
+
+    public void testSetX() throws Exception {
+
+    }
+
+    public void testGetY() throws Exception {
+
+    }
+
+    public void testSetY() throws Exception {
+
+    }
+
+    public void testGetZ() throws Exception {
+
+    }
+
+    public void testSetZ() throws Exception {
+
+    }
+
+    public void testGetA() throws Exception {
+
+    }
+
+    public void testSetA() throws Exception {
+
+    }
+
+    public void testGetB() throws Exception {
+
+    }
+
+    public void testSetB() throws Exception {
+
+    }
+
+    public void testGetC() throws Exception {
+
+    }
+
+    public void testSetC() throws Exception {
+
+    }
+
+    public void testShift() throws Exception {
+
+    }
+
+    public void testShift1() throws Exception {
+
+    }
+
+    public void testClone() throws Exception {
+
+    }
+
+    public void testDistance() throws Exception {
+
+    }
+
+    public void testToString() throws Exception {
+
+    }
+
+    public void testGetCrossLineNLine() throws Exception {
+
+    }
+
+    public void testGetCrossLineNArc() throws Exception {
+
+    }
+
+    public void testGetCrossArcNArc() throws Exception {
+
+    }
+}
